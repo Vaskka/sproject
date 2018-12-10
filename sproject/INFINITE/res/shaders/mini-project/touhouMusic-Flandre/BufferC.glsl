beae34b5fa9de06926f2f98cf53228e7020198e3
ikutoo <1007833641@qq.com>
2018-12-11 01:26:54 +0800

    minors changes

--- sproject/INFINITE/res/shaders/mini-project/touhouMusic-Flandre/BufferC.glsl
+++ sproject/INFINITE/res/shaders/mini-project/touhouMusic-Flandre/BufferC.glsl
@@ -1,18 +1,24 @@
 #define SOUND_DURATION 366
-#define HEIGHT 0.1
+#define HEIGHT 0.02
 #define PI 3.1415926
 
-vec4 circle(vec2 uv, vec2 pos, float rad, vec3 color) 
+//-----------------------------------------------------------------------------------------------------------------------------------
+//2D distance functions from iq's article: http://www.iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
+float sdLine( in vec2 p, in vec2 a, in vec2 b )
 {
-	float d = length(pos - uv) - rad;
-	float t = (d < 0 ? 1.0 : rad / d);
-	t = clamp(t, 0.0, 1.0);
-	return vec4(color, t);
+    vec2 pa = p-a, ba = b-a;
+    float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+    return length( pa - ba*h );
 }
 
-vec4 rangeCircle(vec2 uv, vec2 pos, float minRad, float maxRad, int divideNum, vec3 color)
+float sdCircle( vec2 p, float r )
 {
-	vec2 v = normalize(pos - uv);
+  return length(p) - r;
+}
+
+vec4 annularSector(vec2 uv, vec2 center, float minRad, float maxRad, int divideNum, vec3 color)
+{
+	vec2 v = normalize(uv - center);
 	float theta = asin(v.y);
 	theta = v.x > 0 ? theta : (-theta + PI);
 	theta += iTime;
@@ -21,31 +27,36 @@ vec4 rangeCircle(vec2 uv, vec2 pos, float minRad, float maxRad, int divideNum, v
 	theta = mod(theta, delta);
 	if (theta < delta * 0.2) return vec4(0.0);
 
-	float d = length(pos - uv);
+	float d = length(uv - center);
 	d = max(d - maxRad, minRad - d);
 
-	float t = (d < 0 ? 1.0 : 0.1 * minRad / d);
-	t = clamp(t, 0.0, 1.0);
+	float t = (d < 0 ? 1.0 : 0.1*minRad/d);
+	return vec4(color, clamp(t, 0.0, 1.0));
+}
 
-	return vec4(color, t);
+vec4 renderPlaybackBar(vec2 uv)
+{
+	float playbackPos = iTime / SOUND_DURATION;
+
+	vec3 baseLineCol = playbackPos > uv.x ? vec3(0.0, 0.8, 0.0) :  vec3(0.8, 0.0, 0.0);
+	float alpha = 0.001 / sdLine(uv,vec2(0.0, HEIGHT),vec2(1.0, HEIGHT));
+	vec4 lineCol = vec4(baseLineCol, alpha);
+
+	float sd = sdCircle(uv-vec2(playbackPos, HEIGHT), 0.002);
+	alpha = sd < 0 ? 1.0 : 0.002 / sd;
+	vec4 circleCol = vec4(vec3(1.0), clamp(alpha, 0.0, 1.0));
+
+	vec4 sectorCol = 2.0 * annularSector( uv, vec2(playbackPos, HEIGHT), 0.01+0.002*sin(iTime), 0.012+0.002*sin(iTime), 20, vec3(0.6) );
+	sectorCol.a *= 0.2;
+
+	vec4 col = lineCol;
+	col = mix(col, circleCol, circleCol.a);
+	col = mix(col, sectorCol, sectorCol.a);
+	return col;
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-	vec2 uv = fragCoord.xy / iResolution.y;
-	float playbackPos = iTime / SOUND_DURATION;
-
-	vec3 baseCol = playbackPos > uv.x ? vec3(0.4, 0.8, 0.0) :  vec3(0.8, 0.4, 0.0);
-	vec4 lineCol = 1.0 * vec4(baseCol, 0.5 * (0.002/abs(uv.y-HEIGHT)));
-
-	vec4 circleCol = 2.0 * circle(uv, vec2(playbackPos, HEIGHT), 0.006, vec3(0.0, 0.6, 1.0));
-	circleCol.a *= 0.4;
-	vec4 circleCol2 = 2.0 * rangeCircle(uv, vec2(playbackPos, HEIGHT), 0.022+0.002*sin(iTime), 0.024+0.002*sin(iTime), 5, vec3(0.8));
-	circleCol2.a *= 0.2;
-
-	vec4 col = lineCol;
-	col = mix(col, circleCol, circleCol.a);
-	col = mix(col, circleCol2, circleCol2.a);
-
-	fragColor = col;
+	vec2 uv = fragCoord / iResolution.x;
+	fragColor = renderPlaybackBar(uv);
 }
